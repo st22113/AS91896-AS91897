7544c8a3fcb17cf837d8039653d781655dfe58d1
st22113 <135674300+st22113@users.noreply.github.com>
2023-08-16 10:59:05 +1200

    Changed Listbox to Treeview
    Changed Listbox to Treeview to enhance the user experience also adding features to clear all entry boxes on submit

--- AS91896_AS9187.py
+++ AS91896_AS9187.py
@@ -1,62 +1,53 @@
-from tkinter import * 
-from tkinter import Label
-from tkinter import Listbox
+from tkinter import *
 from tkinter import messagebox
 from tkinter.messagebox import askyesno
+from tkinter import ttk
 
+# Commands
 
-# window created 
-main_window = Tk()
-main_window.geometry("700x680")
-main_window.title("Julie's Party Hire")
-
-#created list box for inputs that have been shown
-listbox = Listbox(main_window, height=15, width=70)
-listbox.grid(row=1, column=0, columnspan=2, padx=(30,30), pady=(20,20)) # listbox dimensions
-listbox.insert(END, "           Full Name       |\tReciept Number       |\tItem Name       |\tQuantity")
-
-
-# commands
-
-#quit - close application
-def quit():
-    yesno = askyesno(title="confirm", message="Are you sure you want to quit")
-    if answer:
+def quit(): # Exits the entire GUI
+    answer = askyesno(title="Confirm", message="Are you sure you want to quit?")
+    if answer: # Confirm Exit
         main_window.destroy()
 
-def submit(): # submits all entry boxes to listbox
+def submit(): # Submits all enteries into the treeveiw 
     name = entry_name.get()
     receipt = entry_receipt.get()
     item = entry_item.get()
     quantity = entry_quantity.get()
-    details = f"{name:<5}\t{receipt:<15}\t{item:<25}\t{quantity}" # sorts the entry into coloums
     
-    if name and receipt and item and quantity: # checks if all boxes are entered
-        details = f"     {name}      {receipt}      {item}      {quantity}"
-        listbox.insert(END, details)
-    
-    if name == "": # Checks if the entry is empty or has a white space
-        messagebox.showerror("Full Name Error", "Please enter your full name")
-
-   
-                
-    
-
-def delete():
-    selected_index = listbox.curselection()
-    if selected_index:
-        index=int(selected_index[0])
-        listbox.delete(index)
+    if name and receipt and item and quantity: # Checks if all entry boxes are filled in
+        tree.insert("", "end", values=(name, receipt, item, quantity))
+        clear_entries()
     else:
-        messagebox.showerror("Error", f"Please Select a Row to Delete")  
+        messagebox.showerror("Entry Error", "Please fill out all fields")
 
-def clear_entries():
+def delete(): # Deletes a selected row in the tree view
+    selected_item = tree.selection() # Selects Row
+    if selected_item:
+        tree.delete(selected_item)
+    else:
+        messagebox.showerror("Error", "Please select a row to delete")
+
+def clear_entries(): # Command to clear entries when submitted 
     entry_name.delete(0,END)
     entry_receipt.delete(0,END)
     entry_item.delete(0,END)
     entry_quantity.delete(0,END)
 
-# labels
+
+# Main Window 
+main_window = Tk()
+main_window.geometry("900x680")
+main_window.title("Julie's Party Hire")
+
+tree = ttk.Treeview(main_window, columns=("Name", "Receipt Number", "Item Name", "Quantity"), show="headings")
+tree.heading("Name", text="Full Name")
+tree.heading("Receipt Number", text="Receipt Number")
+tree.heading("Item Name", text="Item Name")
+tree.heading("Quantity", text="Quantity of Items")
+tree.grid(row=1, column=0, columnspan=2, padx=(30, 30), pady=(20, 20))
+
 label_window = Label(main_window, text="Julie's Party Hire", font="helvetica 35 bold")
 label_name = Label(main_window, text="Full Customer Name:")
 label_receipt = Label(main_window, text="Receipt Number:")
@@ -64,14 +55,11 @@ label_item = Label(main_window, text="Item Name:")
 label_quantity = Label(main_window, text="Quantity of Items:")
 label_window.grid(row=2, column=0, columnspan=2)
 
-
-# Grid 
 label_name.grid(row=3, column=0, pady=5, padx=20, sticky="e")
 label_receipt.grid(row=4, column=0, pady=5, padx=20, sticky="e")
 label_item.grid(row=5, column=0, pady=5, padx=20, sticky="e")
 label_quantity.grid(row=6, column=0, pady=5, padx=20, sticky="e")
 
-# User Entry
 entry_name = Entry(main_window)
 entry_receipt = Entry(main_window)
 entry_item = Entry(main_window)
@@ -83,15 +71,11 @@ entry_item.grid(row=5, column=1, pady=5, padx=5, sticky="w")
 entry_quantity.grid(row=6, column=1, pady=5, padx=5, sticky="w")
 
 btn_submit = Button(main_window, text="Submit", command=submit)
-btn_delete = Button(main_window, text="Delete Selected", command=delete)    
-btn_clear = Button(main_window, text="Clear Entries", command=clear_entries)
-btn_quit=Button(main_window, text="Quit", width=6, command=quit)
+btn_delete = Button(main_window, text="Delete Selected", command=delete)
+btn_quit = Button(main_window, text="Quit", width=6, command=quit)
 
 btn_submit.grid(row=7, column=1, padx=5, pady=10, sticky="w")
 btn_delete.grid(row=3, column=1, padx=5, pady=10, sticky="e")
-btn_clear.grid(row=5, column=1, padx=5, pady=10, sticky="e")
 btn_quit.grid(row=8, column=1, padx=5, pady=10, sticky="e")
-
-
 
 main_window.mainloop()
